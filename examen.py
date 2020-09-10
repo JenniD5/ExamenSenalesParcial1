3e68289fb89b1696f00f60a2573a53d5798c4e94
JenniD5 <elizabethduran0105@gmail.com>
2020-09-10 07:51:16 -0700

    comentarios

--- examen.py
+++ examen.py
@@ -3,7 +3,7 @@ sys.path.insert(1,'dsp-modulo')
 
 from thinkdsp import SinSignal
 from thinkdsp import decorate
-
+#7533
 #sonido 7
 frecuencia852 = SinSignal(freq=852, amp=1, offset=0)
 frecuencia1209 = SinSignal(freq=1209, amp=1, offset=0)
@@ -13,7 +13,7 @@ wave_1209 = frecuencia1209.make_wave(duration=0.5, start=0, framerate=44100)
 
 wave_sonido7 = wave_852 + wave_1209
 
-#sonido5
+#sonido 5
 frecuencia770 = SinSignal(freq=770, amp=1, offset=0)
 frecuencia1336 = SinSignal(freq=1336, amp=1, offset=0)
 
@@ -22,7 +22,7 @@ wave_1336 = frecuencia1336.make_wave(duration=0.5, start=0.5, framerate=44100)
 
 wave_sonido5 = wave_770 + wave_1336
 
-#sonido3
+#sonido 3
 frecuencia697 = SinSignal(freq=697, amp=1, offset=0)
 frecuencia1477 = SinSignal(freq=1477, amp=1, offset=0)
 
